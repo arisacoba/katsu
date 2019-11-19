489b924cf42e6a0fdacea5da4a3e5249471ef3a0
Aris Acoba <aris.acoba@gmail.com>
2019-11-19 11:24:13 +0800

    Add data in app.js

--- docs/assets/app.js
+++ docs/assets/app.js
@@ -1,8 +1,11 @@
 const $imgSrc = document.querySelector('[data-js-img-src]');
-const $toyotoCorolla = document.querySelector('[data-js-toyota-corolla]');
-const $skinWhite = document.querySelector('[data-js-skinwhite-lotion]');
-const $maxipeelZero = document.querySelector('[data-js-maxipeel-zero]');
-const $smdc = document.querySelector('[data-js-smdc]');
-const $smNorth = document.querySelector('[data-js-sm-north]');
-const $sosro = document.querySelector('[data-js-sosro]');
-const $auf = document.querySelector('[data-js-auf]');
+const $imgTrigger = document.querySelectorAll('[data-js-image-trigger]');
+let $imgValue = "";
+
+for(let i=0; i < $imgTrigger.length; i++){
+  $imgTrigger[i].addEventListener('mouseover', function(event){
+    $imgValue = $imgTrigger[i].getAttribute('data-js-image-value');
+    $imgSrc.src="assets/brands/"+$imgValue+".jpg";
+    console.log($imgValue);
+  });
+}
