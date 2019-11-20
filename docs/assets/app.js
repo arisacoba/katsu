b302d69dfdcd139fa2f1c6150c69acd25f940e2e
Aris Acoba <aris.acoba@gmail.com>
2019-11-20 08:59:46 +0800

    Add js for floating image

--- docs/assets/app.js
+++ docs/assets/app.js
@@ -1,11 +1,16 @@
-const $imgSrc = document.querySelector('[data-js-img-src]');
-const $imgTrigger = document.querySelectorAll('[data-js-image-trigger]');
+const $imgFloating = document.querySelector('[data-js-img-floating]');
+const $imgTrigger = document.querySelectorAll('[data-js-image-trigger]'); const $imgSrc = document.querySelector('[data-js-img-src]');
 let $imgValue = "";
 
 for(let i=0; i < $imgTrigger.length; i++){
   $imgTrigger[i].addEventListener('mouseover', function(event){
+    $imgFloating.classList.add('-show');
     $imgValue = $imgTrigger[i].getAttribute('data-js-image-value');
-    $imgSrc.src="assets/brands/"+$imgValue+".jpg";
-    console.log($imgValue);
+    $imgSrc.src="assets/images/brands/"+$imgValue+".jpg";
   });
+  $imgTrigger[i].addEventListener('mouseleave', function(event){
+    $imgFloating.classList.remove('-show');
+    $imgSrc.src="";
+  });
+
 }
